928755b1d675b0a9b0ac51dad5a469a38936401e
valencia4414 <valencia4414@gmail.com>
2026-09-04 12:27:35 -0500

    Revert "Integrar carrito con sesión"
    This reverts commit 7026e60bb52731a24940ac7d1527911c3c15d7e0.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,6 @@ Html
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'FoodPass')</title>
     <!-- Tailwind y Fuentes (RNF10 - Consistencia) -->
     <script src="https://cdn.tailwindcss.com"></script>
